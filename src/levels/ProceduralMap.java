fe4b060d7cf24da892f1bcc080b9add6b76cad8e
Robin Campos <rcampos@unm.edu>
2016-10-17 21:04:01 -0600

    Obstacles now appear as if it were 2 difficulty levels higher

--- src/levels/ProceduralMap.java
+++ src/levels/ProceduralMap.java
@@ -452,7 +452,7 @@ public class ProceduralMap
             } else {
               map[y+1][x+1] = new Tile(r.region,y+1,x+1, !r.isRoom);
               if (x%2 == 1 && y%2 == 1 && r.isRoom){
-                double chance = (difficulty + 1)/7d;
+                double chance = (difficulty + 2)/7d;
                 if (Math.random() < chance){
                   map[y+1][x+1] = new Tile(r.region + 5,y+1,x+1, !r.isRoom); //create decorative obstacles
                 }
